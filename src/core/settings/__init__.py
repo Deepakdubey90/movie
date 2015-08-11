a1b13cc1f7750614cea49b7fd7d189529454582b
Deepak <deepakkumardubey15@gmail.com>
2015-08-11 18:22:49 +0530

    updated

--- src/core/settings/__init__.py
+++ src/core/settings/__init__.py
@@ -14,6 +14,7 @@ import os
 import datetime
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+ALLOWED_HOSTS = ['*']
 
 # Include the right settings
 try:
--- src/core/settings/production.py
+++ src/core/settings/production.py
@@ -6,6 +6,9 @@ SECRET_KEY = 'wa9g_mf%mrairp7#c#&&5pt0y4+5pk2skbizk!$c5km5lbzs^&'
 
 DEBUG = False
 
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+"""
 connection = dj_database_url.parse(os.environ.get("DATABASE_URL"))
 connection.update({
     'CONN_MAX_AGE': 600,
@@ -13,5 +16,5 @@ connection.update({
 DATABASES = {
     "default": connection,
 }
-
+"""
 ALLOWED_HOSTS = [os.environ.get("HOST", "*"), ]
