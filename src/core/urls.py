cbe65882849a59828f17e53762767f46b2ad81f1
Deepak <deepakkumardubey15@gmail.com>
2015-08-12 12:29:09 +0530

    commit

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from .views import IndexView
 from django.views.generic import TemplateView
 from django.views.static import serve
 from django.conf import settings
@@ -12,6 +13,7 @@ document_root = os.path.join(
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', IndexView.as_view()),
     url(r'^api/', include('services.urls', namespace='services')),
     url(r'^static/(?P<path>.*)$', serve, {'document_root': document_root}),
 ]
